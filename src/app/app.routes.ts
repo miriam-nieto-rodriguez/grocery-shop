4304fdc75737155c0e73d4e9c0c8513d63e6be55
Miriam Nieto <miriam.nito726@gmail.com>
2026-03-29 13:01:16 +0200

    Rutas creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: HomeComponent},
+    {path: "product-detail", component: ProductDetailComponent},
+    {path: "product-form", component: ProductFormComponent},
+    {path: "cart", component: CartComponent},
+    {path: "**", redirectTo: "home"}
+];
