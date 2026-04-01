0e9b233f7b4b11363a1a5c94143a0148c2849647
Miriam Nieto <miriam.nito726@gmail.com>
2026-04-01 09:35:03 +0200

    Interfaz y servicios definidos. Y creación del componente home y productCard

--- src/app/interfaces/iproduct.interface.ts
+++ src/app/interfaces/iproduct.interface.ts
@@ -1,2 +1,7 @@
 export interface IProduct {
+    id: number;
+    name: string;
+    category: string;
+    price: number;
+    imageUrl: string;
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { IProduct } from '../interfaces/iproduct.interface';
+import { PRODUCTS } from '../products.db';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  
+  private products: IProduct[] = PRODUCTS;
+
+  getAll(): IProduct[] {
+    return this.products;
+  }
+
+  addProduct(product: IProduct) {
+    this.products.push(product);
+  }
 }
+
