5f48b6e5fe816a46774e5569ffe741e7b28dce0c
Miriam Nieto <miriam.nito726@gmail.com>
2026-04-01 20:32:52 +0200

    Carrito terminado

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  cartServices = inject(CartService);
+  carrito = this.cartServices.carrito;
+
+  getTotal() {
+    return this.carrito().reduce((total, item) => total + item.product.price * item.quantity, 0);
+  }
+
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { IProduct } from '../interfaces/iproduct.interface';
+import { IProduct, IProductItem } from '../interfaces/iproduct.interface';
 import { PRODUCTS } from '../products.db';
 
 @Injectable({
@@ -7,13 +7,32 @@ import { PRODUCTS } from '../products.db';
 })
 export class CartService {
   private cart = signal<IProduct[]>(PRODUCTS);
-
-  addToCart(product: IProduct){
-    this.cart.update(currentCart => [...currentCart, product]); 
-  } 
+  carrito = signal<IProductItem[]>([]);
 
   getCart() {
     return this.cart();
   }
 
+  addToCart(product: IProduct) {
+
+    const item = this.carrito().find(item => item.product.id === product.id);
+
+    if (item) {
+      this.carrito.update(current => current.map(i => i.product.id === product.id ? { ...i, quantity: i.quantity + 1 } : i)) // recorremos todos los item con map, si el id del producto coincide con el id del item que queremos agregar, entonces actualizamos la cantidad sumando 1, sino dejamos el item igual
+    } else {
+      this.carrito.update(current => [...current, { product, quantity: 1 }]); // si el producto no existe en el carrito, lo agregamos con una cantidad de 1
+    }
+  }
+
+  decreaseQuantity(product: IProduct) {
+    const item = this.carrito().find(item => item.product.id === product.id);
+
+    if (item?.quantity === 1) {
+      this.carrito.update(current => current.filter(i => i.product.id !== product.id)) // si la cantidad del producto es 0, lo eliminamos del carrito
+
+    } else {
+      this.carrito.update(current => current.map(i => i.product.id === product.id ? { ...i, quantity: i.quantity - 1 } : i))
+    }
+
+  }
 }
