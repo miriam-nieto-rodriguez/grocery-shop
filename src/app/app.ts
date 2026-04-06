81a0d4a6c68f3eecaadebd6933bd45de9b513cd6
Miriam Nieto <miriam.nito726@gmail.com>
2026-04-06 13:34:10 +0200

    He creado toda la lógica del componente product-detail y le he dado estilos Por otro lado, he añadido 8 productos más al json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgxSonnerToaster],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
