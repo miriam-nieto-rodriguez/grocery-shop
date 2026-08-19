ce0a0a651665d10bc515711d3895e8b75fc3d14b
Miriam Nieto <miriam.nieto726@gmail.com>
2026-08-19 13:53:45 +0200

    Reorganizar proyecto en front, back y BD

---
+++ back/src/app.js
@@ -0,0 +1,27 @@
+// Creation and configuration of the Express APP
+const express = require("express");
+
+
+const app = express();
+app.use(express.json());
+
+
+// Route configuration
+// Example:
+// const apiRoutes = require('./routes/api.routes');
+// app.use('/api', apiRoutes);
+
+// 404 handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "Not found"
+    });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
