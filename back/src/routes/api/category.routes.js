24e3316f0ecaa4328e5f0eb8bedae685390507cf
Miriam Nieto <miriam.nito726@gmail.com>
2026-09-08 12:45:53 +0200

    Añadir validaciones con yup para productos y categorías

--- back/src/routes/api/category.routes.js
+++ back/src/routes/api/category.routes.js
@@ -1,12 +1,14 @@
 const router = require ('express'). Router();
 
+const categorySchema = require('../../../schemas/category.schema');
 const { getAll, getById, create, edit, remove } = require('../../controllers/category.controller');
 const { verifyToken } = require('../../middlewares/auth.middleware');
+const validateSchema = require('../../middlewares/validation.middleware');
 
 router.get('/', getAll);
 router.get('/:categoryId', getById)
-router.post('/', verifyToken ,create)
-router.put('/:categoryId', verifyToken, edit)
+router.post('/', verifyToken, validateSchema(categorySchema),create)
+router.put('/:categoryId', verifyToken, validateSchema(categorySchema),edit)
 router.delete('/:categoryId', verifyToken, remove)
 
 module.exports = router
--- back/src/routes/api/products.routes.js
+++ back/src/routes/api/products.routes.js
@@ -1,13 +1,15 @@
 const router = require ('express'). Router();
 
+const productSchema = require('../../../schemas/product.schema');
 const { getAll, getById, create, edit, remove, assignCategories } = require('../../controllers/product.controller');
 const { verifyToken } = require('../../middlewares/auth.middleware');
+const validateSchema = require('../../middlewares/validation.middleware');
 
 router.get('/', getAll);
 router.get('/:productId', getById)
-router.post('/', verifyToken,create)
-router.post('/:productId/categories', verifyToken, assignCategories)
-router.put('/:productId', verifyToken, edit)
+router.post('/', verifyToken, validateSchema(productSchema) ,create)
+router.post('/:productId/categories', verifyToken,assignCategories)
+router.put('/:productId', verifyToken, validateSchema(productSchema),edit)
 router.delete('/:productId', verifyToken, remove)
 
 module.exports = router;
