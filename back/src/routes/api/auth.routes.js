f3132d6a4fc4c8f16434a2935ff6a8e6eb5983f5
Miriam Nieto <miriam.nito726@gmail.com>
2026-09-08 13:21:36 +0200

    Añadir validaciones con yup para auth

--- back/src/routes/api/auth.routes.js
+++ back/src/routes/api/auth.routes.js
@@ -1,8 +1,10 @@
 const router = require ('express'). Router();
 
+const { registerSchema, loginSchema } = require('../../../schemas/auth.schema');
 const { register, login } = require('../../controllers/auth.controller');
+const validateSchema = require('../../middlewares/validation.middleware');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateSchema(registerSchema),register);
+router.post('/login', validateSchema(loginSchema), login);
 
 module.exports = router;
